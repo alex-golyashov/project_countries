0fb96fcdede2964341636bba96b371ef6904e47c
student <alex.golyashov@gmail.com>
2022-10-29 01:20:01 +0300

    “project_countries1”

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, Http404
 from django.core.paginator import Paginator
 
 def main_page(request):
